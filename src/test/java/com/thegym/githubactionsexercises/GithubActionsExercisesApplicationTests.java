13fc0b305634fb843805a321e530831241627e08
manzitresor <manzitre@gmail.com>
2026-02-04 19:56:18 +0200

    fix: remove failing test

--- src/test/java/com/thegym/githubactionsexercises/GithubActionsExercisesApplicationTests.java
+++ src/test/java/com/thegym/githubactionsexercises/GithubActionsExercisesApplicationTests.java
@@ -3,14 +3,11 @@ package com.thegym.githubactionsexercises;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
 class GithubActionsExercisesApplicationTests {
 
     @Test
     void contextLoads() {
-        assertEquals(1,2);
     }
 
 }

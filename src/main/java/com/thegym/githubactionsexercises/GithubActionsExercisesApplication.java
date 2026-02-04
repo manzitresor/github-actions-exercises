f0e8654efd77e2b1ea4a0fbdc7270b0cf486f8b5
manzitresor <manzitre@gmail.com>
2026-02-04 16:23:50 +0200

    initial commit

---
+++ src/main/java/com/thegym/githubactionsexercises/GithubActionsExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.thegym.githubactionsexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thegym/githubactionsexercises/GithubActionsExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegym.githubactionsexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

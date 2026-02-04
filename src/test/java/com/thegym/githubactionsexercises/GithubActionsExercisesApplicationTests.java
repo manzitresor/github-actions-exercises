e6210f21985d6e5608fb85b3b751f6b3b37cb16e
manzitresor <manzitre@gmail.com>
2026-02-04 19:49:53 +0200

    add unit test workflow

--- src/test/java/com/thegym/githubactionsexercises/GithubActionsExercisesApplicationTests.java
+++ src/test/java/com/thegym/githubactionsexercises/GithubActionsExercisesApplicationTests.java
@@ -3,11 +3,14 @@ package com.thegym.githubactionsexercises;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class GithubActionsExercisesApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(1,2);
     }
 
 }
